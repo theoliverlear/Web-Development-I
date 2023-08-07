0b25aba82773e86b99f72175d21816d50105b103
theoliverlear <sigwartholiver2@gmail.com>
2023-08-07 15:45:12 -0500

    Start Assignment 3

---
+++ Asisignment Three/global-script.js
@@ -0,0 +1,8 @@
+// Oliver Sigwarth
+// CIDS 225 - Web Development I
+// Assignment Two | Personal Portfolio
+// * The objective of this assignment is to create a portfolio website that
+// * showcases your skills and projects. You will use HTML to structure the
+// * content and CSS to style and layout the website.
+// Created: 8/6/2023
+// Last Modified: 8/6/2023
